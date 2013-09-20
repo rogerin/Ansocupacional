dc8568fa376c9c46825aae9ae36fbe028123ae20
Rogerio Alencar <geriofiho@gmail.com>
2013-09-20 18:03:26 -0300

    teste

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -5,11 +5,15 @@ class PageController < ApplicationController
 
     if session[:user]
       if session[:user_tipo]  == 1
+        
         @usuarios = User.count
         @empresas = Empresa.count
         @funcionarios = Funcionario.count
         @exames = Consulta.count
+      
+
       elsif session[:user_tipo] == 2      
+        
         @emp = Empresa.where(:user_id => session[:user_id])
         @empresas = @emp.count
         @exames = Consulta.where(:empresa_id => @emp).count
